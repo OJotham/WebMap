c9c29fe61c4a3b2279ba419bfd25446c14ac1ee2
OJotham <otiangajotham@icloud.com>
2018-07-02 14:55:31 +0300

    Added comments, modified charts and html

--- acquiredarea/admin.py
+++ acquiredarea/admin.py
@@ -5,12 +5,10 @@ from . import models as acquiredarea_parc
 
 # Register your models here.
 class KairiParcelsAdmin(LeafletGeoAdmin):
-	# Remaining_Area = 'remm_area' 
 	list_display = ( 'parcelID', 'owner_name', 'Initial_Area', 'Remaining_Area')
 
 admin.site.register(acquiredarea_parc.Acquiredarea, LeafletGeoAdmin)
 #admin.site.register(acquiredarea_parc.KairiParcels, LeafletGeoAdmin)
 admin.site.register(acquiredarea_parc.KairiParcels, KairiParcelsAdmin)
-#admin.site.register(acquiredarea_parc.Acquiredarea, admin.GeoModelAdmin)
-#admin.site.register(acquiredarea_parc.KairiParcels, admin.GeoModelAdmin)
+
 
